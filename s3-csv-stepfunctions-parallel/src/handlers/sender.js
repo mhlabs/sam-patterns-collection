6176165c171236e97a7757d822a306b7e21eeeb2
Lars Jacobsson <lars.jacobsson@mathem.se>
2021-05-27 23:37:10 +0200

    Update sender.js

--- s3-csv-stepfunctions-parallel/src/handlers/sender.js
+++ s3-csv-stepfunctions-parallel/src/handlers/sender.js
@@ -1,5 +1,3 @@
-const calculation = require('../functions/calculator');
-const toSNS = require('../functions/toSNS');
 const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3();
 exports.handler = async function (event, context) {
