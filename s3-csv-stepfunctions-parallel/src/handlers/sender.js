5100c2c8dc03692787e0128f87717e14e91d2245
Lars Jacobsson <lars.jacobsson@mathem.se>
2021-05-27 23:36:40 +0200

    Adding s3-csv-stepfunctions-parallel/src/handlers/sender.js

---
+++ s3-csv-stepfunctions-parallel/src/handlers/sender.js
@@ -0,0 +1,77 @@
+const calculation = require('../functions/calculator');
+const toSNS = require('../functions/toSNS');
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3();
+exports.handler = async function (event, context) {
+  const query = 'SELECT * FROM S3Object s';
+  const bucket = event.Bucket;
+  const key = event.Key;
+  const params = {
+    Bucket: bucket,
+    Key: key,
+    ExpressionType: 'SQL',
+    Expression: query,
+    ScanRange: {
+      Start: event.StartByte,
+      End: Math.min(
+        event.StartByte + parseInt(process.env.ChunkBytes),
+        event.ContentLength
+      )
+    },
+    InputSerialization: {
+      CSV: {
+        FileHeaderInfo: 'USE'
+      }
+    },
+    OutputSerialization: {
+      JSON: {
+        RecordDelimiter: ','
+      }
+    }
+  };
+
+  const results = await selectFromS3(params);
+  const resultArray = await calculateScores(results);
+  for (const msg of resultArray) {
+    //handle item
+  }
+  console.log(`${resultArray.length} items processed`);
+};
+
+async function selectFromS3(params) {
+  return new Promise((resolve, reject) => {
+    s3.selectObjectContent(params, (err, data) => {
+      console.log(err);
+      if (err) {
+        reject(err);
+      }
+      if (!data) {
+        reject('Empty data object');
+      }
+      const records = [];
+      data.Payload.on('data', (event) => {
+        if (event.Records) {
+          records.push(event.Records.Payload);
+        }
+      })
+        .on('error', (err) => {
+          reject(err);
+        })
+        .on('end', () => {
+          let recordsString = Buffer.concat(records).toString('utf8');
+          recordsString = recordsString.replace(/\,$/, '');
+          recordsString = `[${recordsString}]`;
+          try {
+            const data = JSON.parse(recordsString);
+            resolve(data);
+          } catch (e) {
+            reject(
+              new Error(
+                `Unable to convert S3 data to JSON object. S3 Select Query: ${params.Expression}`
+              )
+            );
+          }
+        });
+    });
+  });
+}
