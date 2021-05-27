e790450159104602ca5815697170a3729be1c7af
Lars Jacobsson <lars.jacobsson@mathem.se>
2021-05-27 23:24:34 +0200

    Adding s3-stepfunctions-parallel-csv-processing/src/handlers/seedSelectRanges.js

---
+++ s3-stepfunctions-parallel-csv-processing/src/handlers/seedSelectRanges.js
@@ -0,0 +1,19 @@
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3();
+exports.handler = async function (event, context) {
+  // We can probably get this cheaper using another object lambda
+  const object = await s3
+    .headObject({ Bucket: event.bucketName, Key: event.key })
+    .promise();
+
+  const startByteList = [];
+  for (let i = 0; i < object.ContentLength; i += parseInt(process.env.ChunkBytes)) {
+    startByteList.push(i);
+  }  
+  return {
+    StartByteList: startByteList,
+    Bucket: event.bucketName,
+    Key: event.key,
+    ContentLength: object.ContentLength
+  };
+};
